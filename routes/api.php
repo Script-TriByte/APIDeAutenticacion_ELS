3df04bb0fbf5ad958f1a0a705f8039fcb6239ce0
Belen Rosas <belenrosas97@gmail.com>
2023-08-24 18:47:56 -0300

    API de autenticacion Co-authored-by: MJoelMartinez <MJoelMartinez@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\UsuarioController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function ()
+{
+    Route::get('/validate',[UsuarioController::class,"ValidateToken"])->middleware('auth:api');
+    Route::get('/logout',[UsuarioController::class,"Logout"])->middleware('auth:api');
+
+
 });
